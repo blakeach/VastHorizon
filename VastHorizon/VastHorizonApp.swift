f64f75f3d2a1ef4807d23e6ce2004af38c24b750
blakeach <blakeach123@gmail.com>
2026-04-29 21:40:45 -0700

    Initial Commit

---
+++ VastHorizon/VastHorizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  VastHorizonApp.swift
+//  VastHorizon
+//
+//  Created by Blake A on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct VastHorizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
